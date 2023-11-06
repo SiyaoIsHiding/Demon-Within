012b00c7a0a4d917c6e72cac48428db0987c783a
Nicolas Assakura Miyazaki <NicolasA@users.noreply.github.com>
2023-11-06 10:55:13 -0800

    Made enemy really like staring at you
    The enemy will now rotate the face the player.

--- Assets/Scripts/BaseEnemyBehavior.cs
+++ Assets/Scripts/BaseEnemyBehavior.cs
@@ -6,7 +6,14 @@ using UnityEngine.AI;
 public class BaseEnemyBehavior : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("This will usually be the player")]
     private GameObject target;
+    [SerializeField]
+    [Tooltip("The half-angle which dictates when the enemy will start to rotate to face the target")]
+    [Range(0, 180)]
+    private float faceTargetLeeway;
+    [SerializeField]
+    private float rotationSpeed;
 
     private NavMeshAgent navMeshAgentComp;
 
@@ -17,6 +24,15 @@ public class BaseEnemyBehavior : MonoBehaviour
 
     void Update()
     {
+        // Move to target
         navMeshAgentComp.SetDestination(target.transform.position);
+
+        // Face the target
+        Vector3 lineToTarget = target.transform.position - transform.position;
+        float angleToTarget = Vector3.Angle(lineToTarget, transform.forward);
+        if (angleToTarget >= faceTargetLeeway)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target.transform.rotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
